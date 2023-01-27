5f68c2b5fcada98b291e224786c00fef0efdf860
ahamm <ahammer753@gmail.com>
2023-01-27 15:12:24 -0600

    completed code along

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,15 @@ array.forEach((item) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   //console.log('Clicked Fav btn')
+   const [, id] =event.target.id.split('--');
+    const index = referenceList.findIndex(taco => taco.id === Number (id))
+
+    referenceList[index].inCart = !referenceList[index].inCart
+    cartTotal();
+    renderCards(referenceList);
+
+
   }
 }
 
@@ -32,6 +40,7 @@ const search = (event) => {
     taco.author.toLowerCase().includes(eventLC) ||
      taco.description.toLowerCase().includes(eventLC)
      );
+     renderCards(serchResult)
 }
 
 // BUTTON FILTER
@@ -82,9 +91,18 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
-  document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
-}
+  const cart = referenceList.filter(taco => taco.inCart);
+  const total = cart.reduce((value1, value2) => value1 + value2.price, 0);
+const free = cart.some(taco => taco.price <= 0 );
+document.querySelector("cartTotal").innerHTML = total.toFixed(2);
+  
+  if (free) {
+    document.querySelector('#includes-free').innerHTML ="INCLUDES FREE ITEMS"
+  } else {
+    document.querySelector('#includes-free').innerHTML = " "
+      }
+  }
+
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
